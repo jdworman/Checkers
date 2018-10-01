0b7b6f06a118e6d39ad957bec636227bcf9e0463
Joseph Worman <jdworman84@gmail.com>
2018-10-01 10:38:41 -0400

    debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_cable.url = "wss://checkers-js.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ["https://checkers-js.herokuapp.com"]
 end
